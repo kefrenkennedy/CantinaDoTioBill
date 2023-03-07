8279d0d01a025767dbc9905e99c5b6bb4dda0259
Kefren Vasconcelos <kefrenkennedyav@gmail.com>
2023-03-07 08:35:30 -0300

    finalized the user's registration form and login

---
+++ LoginRegister.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace CantinaDoTioBill
+{
+    partial class LoginRegister
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmailLogin = new System.Windows.Forms.TextBox();
+            this.txtPasswordLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNameRegister = new System.Windows.Forms.TextBox();
+            this.txtPasswordRegister = new System.Windows.Forms.TextBox();
+            this.txtEmailRegister = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUserType = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtComplement = new System.Windows.Forms.TextBox();
+            this.txtStreetNumber = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtNeighborhood = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEmailLogin
+            // 
+            this.txtEmailLogin.Location = new System.Drawing.Point(56, 176);
+            this.txtEmailLogin.Name = "txtEmailLogin";
+            this.txtEmailLogin.Size = new System.Drawing.Size(188, 27);
+            this.txtEmailLogin.TabIndex = 0;
+            // 
+            // txtPasswordLogin
+            // 
+            this.txtPasswordLogin.Location = new System.Drawing.Point(56, 231);
+            this.txtPasswordLogin.Name = "txtPasswordLogin";
+            this.txtPasswordLogin.PasswordChar = '*';
+            this.txtPasswordLogin.Size = new System.Drawing.Size(188, 27);
+            this.txtPasswordLogin.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(104, 276);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(96, 33);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(-1, -2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cantina Do Tio Bill";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtNameRegister
+            // 
+            this.txtNameRegister.Location = new System.Drawing.Point(416, 146);
+            this.txtNameRegister.Name = "txtNameRegister";
+            this.txtNameRegister.Size = new System.Drawing.Size(139, 27);
+            this.txtNameRegister.TabIndex = 2;
+            // 
+            // txtPasswordRegister
+            // 
+            this.txtPasswordRegister.Location = new System.Drawing.Point(561, 207);
+            this.txtPasswordRegister.Name = "txtPasswordRegister";
+            this.txtPasswordRegister.PasswordChar = '*';
+            this.txtPasswordRegister.Size = new System.Drawing.Size(139, 27);
+            this.txtPasswordRegister.TabIndex = 0;
+            // 
+            // txtEmailRegister
+            // 
+            this.txtEmailRegister.Location = new System.Drawing.Point(561, 146);
+            this.txtEmailRegister.Name = "txtEmailRegister";
+            this.txtEmailRegister.Size = new System.Drawing.Size(139, 27);
+            this.txtEmailRegister.TabIndex = 1;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(505, 365);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(115, 36);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(513, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sign Up";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(457, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(597, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(586, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 21);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(457, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 21);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Type";
+            // 
+            // txtUserType
+            // 
+            this.txtUserType.Location = new System.Drawing.Point(416, 207);
+            this.txtUserType.Name = "txtUserType";
+            this.txtUserType.Size = new System.Drawing.Size(139, 27);
+            this.txtUserType.TabIndex = 10;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(491, 267);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(140, 27);
+            this.txtCity.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(114, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 25);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Sign In";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(544, 243);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 21);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "city";
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(350, 321);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(136, 27);
+            this.txtStreet.TabIndex = 14;
+            // 
+            // txtComplement
+            // 
+            this.txtComplement.Location = new System.Drawing.Point(638, 321);
+            this.txtComplement.Name = "txtComplement";
+            this.txtComplement.Size = new System.Drawing.Size(138, 27);
+            this.txtComplement.TabIndex = 15;
+            // 
+            // txtStreetNumber
+            // 
+            this.txtStreetNumber.Location = new System.Drawing.Point(494, 321);
+            this.txtStreetNumber.Name = "txtStreetNumber";
+            this.txtStreetNumber.Size = new System.Drawing.Size(137, 27);
+            this.txtStreetNumber.TabIndex = 16;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(349, 267);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(136, 27);
+            this.txtState.TabIndex = 17;
+            // 
+            // txtNeighborhood
+            // 
+            this.txtNeighborhood.Location = new System.Drawing.Point(637, 267);
+            this.txtNeighborhood.Name = "txtNeighborhood";
+            this.txtNeighborhood.Size = new System.Drawing.Size(138, 27);
+            this.txtNeighborhood.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(395, 243);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 21);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "state";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(645, 243);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(123, 21);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Neighborhood";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(389, 301);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(57, 21);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Street";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(654, 301);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(114, 21);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "Complement";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(501, 297);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(124, 21);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "Street Number";
+            // 
+            // LoginRegister
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtNeighborhood);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtStreetNumber);
+            this.Controls.Add(this.txtComplement);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtUserType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPasswordLogin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmailLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNameRegister);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtPasswordRegister);
+            this.Controls.Add(this.txtEmailRegister);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "LoginRegister";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmailLogin;
+        private System.Windows.Forms.TextBox txtPasswordLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNameRegister;
+        private System.Windows.Forms.TextBox txtPasswordRegister;
+        private System.Windows.Forms.TextBox txtEmailRegister;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUserType;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtComplement;
+        private System.Windows.Forms.TextBox txtStreetNumber;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtNeighborhood;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Welcome.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CantinaDoTioBill
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.welcomeLabel.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.welcomeLabel.Location = new System.Drawing.Point(216, 200);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(350, 32);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "mensagemDeBoasVindas";
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcomeLabel);
+            this.Name = "Welcome";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Welcome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeLabel;
+    }
+}
